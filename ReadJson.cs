210c7b67d2d3502fc753c4b37baa9cc9b6fcaa84
Austin Gregory <awfulmedia@gmail.com>
2015-10-11 10:01:12 -0400

    Initial commit

---
+++ ReadJson.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.IO;
+
+// We're using the LitJson namespace!
+using LitJson;
+
+public class ReadJson : MonoBehaviour {
+    // String that we load our raw text data into
+    private string jsonString;
+
+    // JsonData object that we use to store the data after we convert it to JSON
+    private JsonData itemData;
+
+	void Start () {
+        /*
+         * 1. Read the JSON file in, store it in jsonString
+         * 2. We then take that JSON-style string and run it through the JsonMapper.ToObect method
+         *    to convert it to a JSON object. (the result can be parsed as a dictionary)
+         */
+        jsonString = File.ReadAllText(Application.streamingAssetsPath + "/Items.json");
+        itemData = JsonMapper.ToObject(jsonString);
+
+        // Example showing how to access certain values from the new JSON object
+        Debug.Log(itemData["Weapons"][1]["name"]);
+
+        // Example showing our GetItem method in practice (pass the name value to the array object, we get the correct response)
+        Debug.Log(GetItem("light_rifle", "Weapons")["power"]);
+	}
+	
+
+    // This handles querying the data to find the item we want
+    JsonData GetItem(string name, string type)
+    {
+        // Let's look through each item of the type value that we gave the method
+        for (int i = 0; i < itemData[type].Count; i++)
+        {
+            // If we find one by that name (or slug) then we'll just simply return it
+            if (itemData[type][i]["name"].ToString() == name || itemData[type][i]["slug"].ToString() == name)
+                return itemData[type][i];
+        }
+
+        // If we didn't find anything, simply return it
+        return null;
+    }
+}
---
+++ WriteJson.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+
+// We're using the LitJson namespace!
+using LitJson;
+
+
+public class WriteJson : MonoBehaviour {
+    // Create a Character instance called player
+    public Character player = new Character(0, "Austin The Cowardly Wizard", 1337, false, new int[] { 7, 4, 8, 2, 5, 4 });
+
+    // JsonData will hold the converted-to-json data
+    private JsonData playerJson;
+    // Use this for initialization
+	void Start () {
+        // Pass JsonMapper.ToJson method the player object (character instance). This converts the data to JSON format
+        playerJson = JsonMapper.ToJson(player);
+
+        // Now write the restulting data to our StreamingAssets folder
+        File.WriteAllText(Application.streamingAssetsPath + "/Player.json", playerJson.ToString());
+	}
+}
+
+// Example class
+public class Character
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public bool Aggressive { get; set; }
+    public int[] Stats { get; set; }
+
+    public Character(int id, string name, int health, bool aggressive, int[] stats)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Health = health;
+        this.Aggressive = aggressive;
+        this.Stats = stats;
+    }
+}
